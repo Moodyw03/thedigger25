5ee90cbff747571729802537c0d1f861014bc888
Muhammad Umar <umarsadiqi404@gmail.com>
2023-09-27 14:59:33 +0500

    saving artist data to file

--- app.py
+++ app.py
@@ -1,112 +1,117 @@
-import requests
-import time
-import json
-import re
-from urllib.parse import quote
-from bs4 import BeautifulSoup
+# import requests
+# import time
+# import json
+# import re
+# from urllib.parse import quote
+# from bs4 import BeautifulSoup
 
-BASE_URL = "https://www.mixesdb.com"
+# BASE_URL = "https://www.mixesdb.com"
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+# HEADERS = {"User-Agent": "Mozilla/5.0"}
 
-def get_artist_data(artist_name):
-    # encoded_input = quote(artist_name)
-    SEARCH_URL = f'https://www.mixesdb.com/db/index.php?title=Special:Search&limit=100&offset=0&profile=mixes&search={artist_name}'
+# def get_artist_data(artist_name):
+#     # encoded_input = quote(artist_name)
+#     SEARCH_URL = f'https://www.mixesdb.com/db/index.php?title=Special:Search&limit=100&offset=0&profile=mixes&search={artist_name}'
 
-    page = requests.get(SEARCH_URL, headers=HEADERS)
+#     page = requests.get(SEARCH_URL, headers=HEADERS)
 
-    if page.status_code!=200:
-        return "Unable to reach the page"
+#     if page.status_code!=200:
+#         return "Unable to reach the page"
 
-    soup = BeautifulSoup(page.content, "html.parser")
+#     soup = BeautifulSoup(page.content, "html.parser")
 
-    page_title_list_element = soup.select_one('#Page_title_matches + .mw-search-results')
+#     page_title_list_element = soup.select_one('#Page_title_matches + .mw-search-results')
 
-    list_of_titles =  page_title_list_element.find_all("a", class_="cat-tlC")
+#     list_of_titles =  page_title_list_element.find_all("a", class_="cat-tlC")
 
-    data = []
+#     data = []
 
-    for anchor in list_of_titles:
-        title = anchor.text
-        link = BASE_URL + anchor["href"]
-        data.append({"title": title, "link": link})
+#     for anchor in list_of_titles:
+#         title = anchor.text
+#         link = BASE_URL + anchor["href"]
+#         data.append({"title": title, "link": link})
 
-    return data
+#     return data
 
-def get_tracklists(artist_data):
-    start_time = time.time()   # start time
+# def get_tracklists(artist_data):
+#     start_time = time.time()   # start time
 
-    counter = 0
+#     counter = 0
 
-    tracks = []
+#     tracks = []
 
-    for artist in artist_data:
-        print(artist["link"])
-        page = requests.get(artist["link"], headers=HEADERS)
-
-        
-        if page.status_code != 200:
-            continue
-
-        soup = BeautifulSoup(page.content, "html.parser")
-        
-        # Find the script tag containing the JavaScript object
-        script_tag = soup.find("script", string=re.compile("mw.config.set"))
-        
-        if not script_tag:
-            continue
-
-        # Extract the JavaScript object string using a regular expression
-        js_object_str = re.search("mw.config.set\((\{.*?\})\);", script_tag.string, re.DOTALL)
-        
-        if not js_object_str:
-            continue
-
-        # Load the JavaScript object string as a Python dictionary
-        js_object = json.loads(js_object_str.group(1))
-        
-        # Extract the desired data
-        encoded_html = js_object.get("pageAffTl", {}).get("tl", "")
-        
-        if not encoded_html:
-            continue
-
-        # Parse the encoded HTML string with BeautifulSoup
-        encoded_soup = BeautifulSoup(encoded_html, "html.parser")
-        
-        # Extract track title and YouTube ID from the parsed HTML
-        for li in encoded_soup.find_all("li"):
-            span = li.find("span")
-            if span:
-                title_of_track = li.text
-                yt_id_of_track = span.get("data-youtubeid") if span else "-1"
-                tracks.append({"titleOfTrack": title_of_track, "ytIdOfTrack": yt_id_of_track})
-                counter = counter + 1
-                print("🔢:", counter)
-                print(title_of_track)
-                print(yt_id_of_track)
-                print("🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️")
-
-    end_time = time.time()   # end time
-    time_lapsed = end_time - start_time  # time lapsed
-    print("Time lapsed to run the function: {} seconds".format(time_lapsed))
-    print("Counted:", counter)
-
-    print(len(tracks))
-        
-    return json.dumps(tracks, indent=2) 
-
+#     for artist in artist_data:
+#         print(artist["link"])
+#         page = requests.get(artist["link"], headers=HEADERS)
 
         
+#         if page.status_code != 200:
+#             continue
+
+#         soup = BeautifulSoup(page.content, "html.parser")
+        
+#         # Find the script tag containing the JavaScript object
+#         script_tag = soup.find("script", string=re.compile("mw.config.set"))
+        
+#         if not script_tag:
+#             continue
+
+#         # Extract the JavaScript object string using a regular expression
+#         js_object_str = re.search("mw.config.set\((\{.*?\})\);", script_tag.string, re.DOTALL)
+        
+#         if not js_object_str:
+#             continue
+
+#         # Load the JavaScript object string as a Python dictionary
+#         js_object = json.loads(js_object_str.group(1))
+        
+#         # Extract the desired data
+#         encoded_html = js_object.get("pageAffTl", {}).get("tl", "")
+        
+#         if not encoded_html:
+#             continue
+
+#         # Parse the encoded HTML string with BeautifulSoup
+#         encoded_soup = BeautifulSoup(encoded_html, "html.parser")
+        
+#         # Extract track title and YouTube ID from the parsed HTML
+#         for li in encoded_soup.find_all("li"):
+#             span = li.find("span")
+#             if span:
+#                 title_of_track = li.text
+#                 yt_id_of_track = span.get("data-youtubeid") if span else "-1"
+#                 tracks.append({"titleOfTrack": title_of_track, "ytIdOfTrack": yt_id_of_track})
+#                 counter = counter + 1
+#                 print("🔢:", counter)
+#                 print(title_of_track)
+#                 print(yt_id_of_track)
+#                 print("🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️🕹️")
+
+#     end_time = time.time()   # end time
+#     time_lapsed = end_time - start_time  # time lapsed
+#     print("Time lapsed to run the function: {} seconds".format(time_lapsed))
+#     print("Counted:", counter)
+
+#     print(len(tracks))
+        
+#     return json.dumps(tracks, indent=2) 
 
 
-# usage
+        
 
-# TODO: make it a user input
-artist_name = input("Enter artist: ")
 
-# artist_name = "Ben UFO"   # replace with user input
-artist_data = get_artist_data(artist_name)
-dataFetched = get_tracklists(artist_data)
-print(dataFetched)
-print(len(dataFetched))
+# # usage
+
+# # TODO: make it a user input
+# artist_name = input("Enter artist: ")
+
+# # artist_name = "Ben UFO"   # replace with user input
+# artist_data = get_artist_data(artist_name)
+# dataFetched = get_tracklists(artist_data)
+# print(dataFetched)
+# print(len(dataFetched))
+
+
+from main import main
+
+main()
--- main.py
+++ main.py
@@ -1,143 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# URL = "https://www.mixesdb.com/w/2008-06-18_-_Ben_UFO_-_FACT_Mix_08"
-# HEADERS = {"User-Agent": "Mozilla/5.0"}
-
-# page = requests.get(URL, headers=HEADERS)
-
-# if page.status_code == 200:
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     # script = soup.find('script')
-#     # script_text = script.text
-
-#     # pattern = re.compile(r'data-youtubeid="([-\w]+)"')
-#     # youtube_ids = re.findall(pattern, script_text)
-
-#     # print(youtube_ids)
-#     print(soup)
-
-#     # Find the element with id 'tracklist'
-#     tracklist = soup.find(id="Tracklist")
-#     # print(tracklist)
-#     # Initialize a list to store the extracted elements
-#     extracted_elements = []
-
-#     # If the 'tracklist' element is found, find all adjacent <dl> and <ol> tags
-#     # until the element with id 'bodyBottom' is encountered
-#     if tracklist:
-#         for sibling in tracklist.find_all_next():
-#             # print(sibling)
-#             # If the sibling has id 'bodyBottom', break the loop
-#             if sibling.get('id') == 'bodyBottom':
-#                 break
-#             # If the sibling is <dl> or <ol>, append it to the list of extracted elements
-#             elif sibling.name in ['dl', 'ol']:
-#                 extracted_elements.append(sibling)
-
-
-#     # Display the extracted elements
-#     for element in extracted_elements:
-#         # print(element.prettify())
-#         li_elements = element.find_all("li")
-#         for ele in li_elements:
-#             print(ele.find("span"))
-#             print(ele.text.strip())
-#             span = ele.find("span")
-#             if span and 'data-youtubeid' in span.attrs:
-#                 print(f"data-youtubeid: {span['data-youtubeid']}")
-#                 print(f"Text: {ele.text.strip()}")
-
-
-# # soup = BeautifulSoup(page.content, "html.parser")
-
-
-# # Find element by ID
-# # results = soup.find(id="ResultsContainer")
-# # print(results.prettify())
-# # python_jobs = results.find_all("h2", string="Python")
-# # python_jobs = results.find_all(
-# #   "h2", string=lambda text: "python" in text.lower()
-# # )
-
-# # python_job_elements = [
-# #   h2_element.parent.parent.parent for h2_element in python_jobs
-# # ]
-
-# # for job_element in python_job_elements:
-# #   title_element = job_element.find("h2", class_="title")
-# #   company_element = job_element.find("h3", class_="company")
-# #   location_element = job_element.find("p", class_="location")
-# #   print(title_element.text.strip())
-# #   print(company_element.text.strip())
-# #   print(location_element.text.strip())
-# #   print()
-# #   links = job_element.find_all("a")
-# #   for link in links:
-# #     link_url = link["href"]
-# #     print(link_url)
-# #   print()
-
-# # Find element by Class
-# # job_elements = results.find_all("div", class_="card-content")
-# # print(job_elements[0].prettify())
-
-# # for job_element in job_elements:
-# #   title_element = job_element.find("h2", class_="title")
-# #   company_element = job_element.find("h3", class_="company")
-# #   location_element = job_element.find("p", class_="location")
-# #   print(title_element.text.strip())
-# #   print(company_element.text.strip())
-# #   print(location_element.text.strip())
-# #   print()
-
-
-# #############
-# import requests
-# import json
-# import re
-# from bs4 import BeautifulSoup
-
-# URL = "https://www.mixesdb.com/w/2008-06-18_-_Ben_UFO_-_FACT_Mix_08"
-# HEADERS = {"User-Agent": "Mozilla/5.0"}
-
-# page = requests.get(URL, headers=HEADERS)
-
-# if page.status_code == 200:
-#     soup = BeautifulSoup(page.content, "html.parser")
-
-#     # Find the script tag containing the JavaScript object
-#     script_tag = soup.find("script", text=re.compile("mw.config.set"))
-
-#     if script_tag:
-#         # Extract the JavaScript object string using a regular expression
-#         js_object_str = re.search("mw.config.set\((\{.*?\})\);", script_tag.string, re.DOTALL)
-
-#         if js_object_str:
-#             # Load the JavaScript object string as a Python dictionary
-#             js_object = json.loads(js_object_str.group(1))
-
-#             # Extract the desired data
-#             encoded_html = js_object.get("pageAffTl", {}).get("tl", "")
-
-#             # Parse the encoded HTML string with BeautifulSoup
-#             encoded_soup = BeautifulSoup(encoded_html, "html.parser")
-
-#             for li in encoded_soup.find_all("li"):
-#                 print(li.text)
-#                 span = li.find("span", {"data-youtubeid": True})
-#                 if span:
-#                     print(span["data-youtubeid"])
-#                     print()
-#                     print(":::::::::::::::::::::::")
-#                     print()
-
-
-# div.explorerResult[] > div.ExplorerTracklist > ol > li > text > strip()
-
-
-# div.explorerResult[] > div.ExplorerTracklist > ol[] > li > text > strip
-
 import requests
 import json
 from urllib.parse import urlencode
@@ -147,84 +7,67 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 total_track_lists = 0
 
-def build_url(do="", mode="", cat1="", cat2="", jnc="", style="", year="",
-              tlc="", tli="", so="", tmatch1="", tmatch2="", jntm="", usesFile="",
-              minHotnessLevel="", count="25", order="name", sort="", offset=""):
-
+def build_url(offset, other_params):
     base_url = "https://www.mixesdb.com/w/MixesDB:Explorer/Mixes"
-
-    # This is a long comment. This should be wrapped to fit within 72 characters.
-    query_params_order = [
-        ("do", do),
-        ("mode", mode),
-        ("cat1", cat1),
-        ("cat2", cat2),
-        ("jnC", jnc),
-        ("style", style),
-        ("year", year),
-        ("tlC", tlc),
-        ("tlI", tli),
-        ("so", so),
-        ("tmatch1", tmatch1),
-        ("tmatch2", tmatch2),
-        ("jnTm", jntm),
-        ("usesFile", usesFile),
-        ("minHotnessLevel", minHotnessLevel),
-        ("count", count),
-        ("order", order),
-        ("sort", sort),
-        ("offset", offset)
-    ]
-
-    query_string = urlencode(query_params_order)
-
+    params = {
+        "do": "mx",
+        "mode": "",
+        "cat1": "Ben UFO",
+        "cat2": "",
+        "jnC": "",
+        "style": "",
+        "year": "",
+        "tlC": "1",
+        "tlI": "1",
+        "so": "",
+        "tmatch1": "",
+        "tmatch2": "",
+        "jnTm": "",
+        "usesFile": "",
+        "minHotnessLevel": "",
+        "count": "25",
+        "order": "name",
+        "sort": "desc",
+        "offset": str(offset),
+        **other_params
+    }
+    query_string = urlencode(params)
     return f"{base_url}?{query_string}"
 
 
-# Usage
-dynamic_url = build_url(do="mx", cat1="Ben UFO", tlc="1", tli="1", sort="desc", offset="1")
-print(dynamic_url)
 
+def fetch_tracklists(offset, other_params):
+    url = build_url(offset, other_params)
+    response = requests.get(url, headers=HEADERS)
+    response.raise_for_status()
+    return BeautifulSoup(response.content, "html.parser")
 
-page = requests.get(dynamic_url, headers=HEADERS)
+def parse_tracklists(soup):
+    tracklists = []
+    for explorerResult in soup.find_all("div", class_="explorerResult"):
+        tracklist = []
+        ol_tags = explorerResult.find_all("ol")
+        for ol_tag in ol_tags:
+            li_tags = ol_tag.find_all("li")
+            for li_tag in li_tags:
+                tracklist.append(li_tag.text.strip())
+        if tracklist:
+            tracklists.append(tracklist)
+    return tracklists
 
-if not page.status_code == 200:
-    print("Not able to fetch the url...")
+def write_to_json(tracklists, filename="tracklists.json"):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(tracklists, f, ensure_ascii=False, indent=4)
 
-soup = BeautifulSoup(page.content, "html.parser")
+def main():
+    total_track_lists = 295  # or you can dynamically find this value
+    all_tracklists = []
+    for offset in range(0, total_track_lists, 25):
+        soup = fetch_tracklists(offset, {})
+        tracklists = parse_tracklists(soup)
+        all_tracklists.extend(tracklists)
+    write_to_json(all_tracklists)
 
-# Get total number of track lists
-exploreRes_class = soup.find("span", class_="explorerRes")
-b_tag_in_explorerRes_class = exploreRes_class.find("b")
-total_track_lists = b_tag_in_explorerRes_class.text.strip()
-
-# Get all the tracks in lists
-explorerResult_classes = soup.find_all("div", class_="explorerResult")
-
-if len(explorerResult_classes) < 1:
-    print("No tracklist found.")
-
-list_counter = 0
-tracks_counter = 0
-
-for explorerResult_class in explorerResult_classes:
-    explorerTracklist_class = explorerResult_class.find("div", class_="ExplorerTracklist")
-    if not explorerTracklist_class:
-        print("this list is skipped.")
-        continue
-    ol_in_explorerTracklist_class_array = explorerTracklist_class.find_all("ol");
-    if len(ol_in_explorerTracklist_class_array) < 1:
-        print("No records in this list...")
-        continue
-    list_counter = list_counter + 1
-    for ol_in_explorerTracklist_class in ol_in_explorerTracklist_class_array:
-        li_in_ol_in_explorerTracklist_class_array = ol_in_explorerTracklist_class.find_all("li")
-        if len(li_in_ol_in_explorerTracklist_class_array) < 1:
-            print("this list is empty")
-        for li_in_ol_in_explorerTracklist_class in li_in_ol_in_explorerTracklist_class_array:
-            print(li_in_ol_in_explorerTracklist_class.text.strip())
-            tracks_counter = tracks_counter + 1
-
-print("Total lists:", list_counter)
-print("Total tracks:", tracks_counter)
+if __name__ == "__main__":
+    main()
     
