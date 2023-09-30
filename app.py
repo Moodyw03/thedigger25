59889570ea2d7b22ff5d7e301949eceff3ae9036
Muhammad Umar <umarsadiqi404@gmail.com>
2023-09-30 18:01:18 +0500

    cors and error handling

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+
 from main import main
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.debug = True
 
 @app.route('/api/list', methods=['GET'])
 def fetch_artists():
-    # Access the 'artist_name' query parameter
-    artist_name = request.args.get('artist_name', 'Ben UFO')
-    arrays = main(artist_name)
+    try:
+        # Access the 'artist_name' query parameter
+        artist_name = request.args.get('artist_name', 'Ben UFO')
+        
+        # Validate artist_name if necessary
+        # e.g., check if it's not empty, has valid characters, etc.
+        
+        arrays = main(artist_name)
 
-    # Use jsonify to return the array of arrays as JSON
-    return jsonify(arrays)
+        # Check if arrays is valid or has data
+        if not arrays:
+            return make_response(jsonify({"error": "No data found for the given " + artist_name}), 404)
+
+        return jsonify(arrays)
+    
+    except ValueError as e:  # Example error
+        return make_response(jsonify({"error": str(e)}), 400)
+    
+    except Exception as e:
+        # Log the error for debugging
+        # print(e)  # Or use logging module for production
+        return make_response(jsonify({"error": "An unexpected error occurred"}), 500)
+
 
 
 if __name__ == '__main__':
